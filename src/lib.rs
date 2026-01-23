0af1a516d49eef92f7a1258b20865894a28bcea7
Antriksh Gwal <antrikshgwal@gmail.com>
2026-01-23 17:17:28 +0530

    Block header parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
-pub fn read_le_bytes(content: &[u8]) {
+fn read_u32_le(data: &[u8], offset: usize) -> u32 {
+    let bytes: [u8; 4] = data[offset..offset + 4].try_into().unwrap();
+
+    u32::from_le_bytes(bytes)
+}
+pub fn match_block(content: &[u8]) {
     let mut offset = 0usize;
 
     while offset + 4 <= content.len() {
-        if read_magic_bytes(content, offset) {
+        if !match_magic_bytes(content, offset) {
             println!("Magic bytes not matched!");
             break;
         }
 
-        let size = &content[offset + 4..offset + 8];
-        let block_size = u32::from_le_bytes(size.try_into().unwrap()) as usize;
+        let size = read_u32_le(content, offset + 4) as usize;
+        let block_size = read_u32_le(content, offset+size) as usize;
         if offset + 8 + block_size > content.len() {
             break;
         }
@@ -16,15 +21,39 @@ pub fn read_le_bytes(content: &[u8]) {
     }
 }
 
-fn read_magic_bytes(content: &[u8], offset: usize) -> bool {
+fn match_magic_bytes(content: &[u8], offset: usize) -> bool {
     let MAGIC: [u8; 4] = [0xf9, 0xbe, 0xb4, 0xd9];
     let magic_array = &content[offset..offset + 4];
     if magic_array != MAGIC {
-        return true;
+        return false;
     }
-    false
+    true
 }
 
-fn parse_header(header: &[u8]){
-  
+
+fn read_32_bytes(data: &[u8], offset: usize) -> [u8; 32] {
+    data[offset..offset + 32]
+        .try_into()
+        .expect("slice must be 32 bytes")
+}
+
+#[derive(Debug)]
+struct Blockheader {
+    version: u32,
+    prev_block: [u8; 32],
+    merkle_root: [u8; 32],
+    time: u32,
+    bits: u32, // target at the time of block mined
+    nonce: u32,
+}
+
+fn parse_header(header: &[u8])-> Blockheader {
+ Blockheader {
+     version : read_u32_le(header, 0),
+     prev_block : read_32_bytes(header, 4),
+     merkle_root : read_32_bytes(header, 36),
+     time : read_u32_le(header, 68),
+     bits : read_u32_le(header, 72),
+     nonce : read_u32_le(header, 76 )
+}
 }
